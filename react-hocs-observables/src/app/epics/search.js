f19946e060c6df235936cf4c214cd2fb8a15406b
iproduct <office@iproduct.org>
2017-05-18 10:22:25 +0300

    search epics completed

--- react-hocs-observables/src/app/epics/search.js
+++ react-hocs-observables/src/app/epics/search.js
@@ -1,41 +1,30 @@
-import { SEARCH, SEARCH_COMPLETE, searchComplete, searchRejected} from '../actions';
+import { SEARCH, SEARCH_COMPLETE, searchComplete, searchRejected } from '../actions';
 import { Observable } from 'rxjs';
 
 const API_PATH = 'https://www.googleapis.com/books/v1/volumes';
 const { ajax } = Observable;
 
-const search = action$ =>
-  action$
+const search = actions$ =>
+  actions$
     .ofType(SEARCH)
     .debounceTime(300)
     .map(action => action.query)
     .distinctUntilChanged()
     .do(query => console.log('Query:', query))
     .distinctUntilChanged()
-    .mergeMap(query =>
-      ajax.getJSON(`${API_PATH}?q=${query}`)
-        .map(response => searchComplete(response.items || []))
-    ).catch(error => Observable.of(
+    .mergeMap(query => {
+      if (query === '') {
+        return Observable.empty();
+      }
+      console.log('Query to complete:', query);
+
+      const nextSearch$ = actions$.ofType(SEARCH).skip(1);
+
+      return ajax.getJSON(`${API_PATH}?q=${query}`)
+        .takeUntil(nextSearch$)
+        .map(response => searchComplete(response.items || []));
+    }).catch(error => Observable.of(
       searchRejected(error.xhr.response)
     ));
-
-/*search$: Observable<Action> = this.actions$
-    .ofType(book.SEARCH)
-    .debounceTime(300)
-    .map(toPayload)
-    .switchMap(query => {
-      if (query === '') {
-        return empty();
-      }
-
-      const nextSearch$ = this.actions$.ofType(book.SEARCH).skip(1);
-
-      return this.googleBooks.searchBooks(query)
-        .takeUntil(nextSearch$)
-        .map(books => new book.SearchCompleteAction(books))
-        .catch(() => of(new book.SearchCompleteAction([])));
-    });*/
-
-
 
 export default search;
